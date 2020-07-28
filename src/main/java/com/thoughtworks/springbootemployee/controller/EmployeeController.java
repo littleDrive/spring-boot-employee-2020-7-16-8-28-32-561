0cc34a5c209b4a74b7111942c9f3b9cd821247ea
littleDrive <2682155146@qq.com>
2020-07-28 22:13:02 +0800

    [employees API] [zero] feature: implement GET       /employees/1  # obtain a certain specific employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -3,9 +3,11 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.model.Database;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
@@ -21,5 +23,16 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         List<Employee> employees = Database.getEmployees();
         return employees;
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeByID(@PathVariable int id) {
+        List<Employee> employees = Database.getEmployees();
+        for(Employee employee : employees) {
+            if (employee.getId() == id) {
+                return employee;
+            }
+        }
+        return null;
     }
 }
