6cb5b60dd9507ec508ca7ba8972443d2780ecab1
littleDrive <2682155146@qq.com>
2020-07-28 22:06:11 +0800

    [employees API] [zero] feature: implement GET       /employees    #obtain employee list

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Database;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author ozm
@@ -12,5 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = Database.getEmployees();
+        return employees;
+    }
 }
