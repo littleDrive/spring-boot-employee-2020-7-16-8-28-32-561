e03e5c6c6a7e4f95cedc5e569c256ad7a2651a81
littleDrive <2682155146@qq.com>
2020-07-28 20:15:09 +0800

    [Company API] [zero] feature: add Employee

--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -1,8 +1,60 @@
 package com.thoughtworks.springbootemployee.model;
 
-/**
- * @author ozm
- * @date 2020/7/28 - 20:09
- */
+import sun.management.Agent;
+
 public class Employee {
+
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private double salary;
+
+    public Employee(int id, String name, int age, String gender, double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
