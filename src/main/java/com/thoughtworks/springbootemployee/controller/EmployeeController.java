a04f65ab30756242f653c45f2b3acce91eda72fd
littleDrive <2682155146@qq.com>
2020-07-28 22:02:29 +0800

    [employees API] [zero] feature: add EmployeeController

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ozm
+ * @date 2020/7/28 - 21:57
+ */
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+
+}
