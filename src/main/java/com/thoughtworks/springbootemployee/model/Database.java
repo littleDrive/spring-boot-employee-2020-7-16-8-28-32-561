018716acb0126d788cbe7ec52225d0011ff08ca2
littleDrive <2682155146@qq.com>
2020-07-28 20:35:57 +0800

    [Company API] [zero] feature: implement GET       /companies    #obtain company list

--- src/main/java/com/thoughtworks/springbootemployee/model/Database.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Database.java
@@ -8,10 +8,11 @@ public class Database {
 
     public static List<Employee> getEmployees() {
         List<Employee> employees = new ArrayList<>();
-        int i = 0;
-        while (i < 5) {
+        int i = 1;
+        while (i <= 5) {
             employees.add(new Employee(i, "alibaba"+i, 22, "male", 5000));
-            employees.add(new Employee(i*2, "alibaba"+i*2, 25, "female", 7000));
+            employees.add(new Employee(i+5, "alibaba"+i*2, 25, "female", 7000));
+            i++;
 
         }
         return employees;
@@ -20,11 +21,11 @@ public class Database {
     public static List<Company> getCompanies() {
 
         List<Company> companies = new ArrayList<>();
-        int i = 0;
-        while (i < 5) {
+        int i = 1;
+        while (i <= 5) {
             companies.add(new Company("alibaba", 10, getEmployees()));
             companies.add(new Company("tengxun", 10, getEmployees()));
-
+            i++;
         }
         return companies;
     }
