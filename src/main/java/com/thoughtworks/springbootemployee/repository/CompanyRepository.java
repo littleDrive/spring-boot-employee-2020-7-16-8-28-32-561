7a6a89e4367cc7783286793d4405ea81393eba22
littleDrive <ZERO.OU@OOCL.COM>
2020-07-29 15:10:45 +0800

    [springboot-employee] [zero/hovees] feature: implement should_return_companies_when_get_companies

---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CompanyRepository {
+
+    public List<Company> getCompanies() {
+        return null;
+    }
+
+}
